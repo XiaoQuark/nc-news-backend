cda0a65a7e1301496e3e156d237311e57c37b3f6
XiaoQuark <kris.oldrini@gmail.com>
2024-05-29 12:02:55 +0200

    complete task 3 GET /api

---
+++ __tests__/app.test.js
@@ -0,0 +1,46 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const endpoints = require("../endpoints.json");
+
+beforeEach(() => {
+    return seed(data);
+});
+
+afterAll(() => db.end());
+
+describe("GET /api", () => {
+    test("should respond with an object describing all available endpoints", () => {
+        return request(app)
+            .get("/api")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.api).toEqual(endpoints);
+            });
+    });
+});
+
+describe("GET /api/topics", () => {
+    test("should respond with an array of topic objects with slug and description properties", () => {
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+            .then(({ body }) => {
+                expect(body.topics.length).toBe(3);
+                body.topics.forEach((topic) => {
+                    expect(typeof topic.slug).toBe("string");
+                    expect(typeof topic.description).toBe("string");
+                });
+            });
+    });
+    test("should respond with a 404 error message when passed a nonexistent endpoint", () => {
+        return request(app)
+            .get("/api/dinosaurs")
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.msg).toBe("404: Not Found");
+            });
+    });
+});
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,10 +1,10 @@
-const { selectTopics } = require("../models/topics.model")
+const { selectTopics } = require("../models/topics.model");
+const endpoints = require("../endpoints.json");
 
 exports.getTopics = (req, res, next) => {
     selectTopics()
-    .then((topicsList) => {
-        res.status(200).send({ topics : topicsList })
-    })
-    .catch(next)
-    
-}
+        .then((topicsList) => {
+            res.status(200).send({ topics: topicsList });
+        })
+        .catch(next);
+};
