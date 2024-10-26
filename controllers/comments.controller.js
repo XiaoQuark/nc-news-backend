5871d1cd5c66d332818f004daac3ef6f3c082812
XiaoQuark <kris.oldrini@gmail.com>
2024-10-26 19:35:41 +0100

    create get user by username request

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,43 +1,46 @@
 const { checkArticleExists } = require("../models/articles.model");
-const { checkUserExists } = require("../models/users.model");
+const { selectUserByUsername } = require("../models/users.model");
 const {
-    selectCommentsByArticleId,
-    insertCommentByArticleId,
-    removeCommentById,
+	selectCommentsByArticleId,
+	insertCommentByArticleId,
+	removeCommentById,
 } = require("../models/comments.model");
 
 exports.getCommentsByArticleId = (req, res, next) => {
-    const { article_id } = req.params;
-    checkArticleExists(article_id)
-        .then(() => {
-            return selectCommentsByArticleId(article_id);
-        })
-        .then((comments) => {
-            res.status(200).send({ comments });
-        })
-        .catch(next);
+	const { article_id } = req.params;
+	checkArticleExists(article_id)
+		.then(() => {
+			return selectCommentsByArticleId(article_id);
+		})
+		.then((comments) => {
+			res.status(200).send({ comments });
+		})
+		.catch(next);
 };
 
 exports.postCommentByArticleId = (req, res, next) => {
-    const { article_id } = req.params;
-    const { username, body } = req.body;
+	const { article_id } = req.params;
+	const { username, body } = req.body;
 
-    Promise.all([checkUserExists(username), checkArticleExists(article_id)])
-        .then(() => {
-            return insertCommentByArticleId(article_id, username, body);
-        })
-        .then((comment) => {
-            res.status(201).send({ comment });
-        })
-        .catch(next);
+	Promise.all([
+		selectUserByUsername(username),
+		checkArticleExists(article_id),
+	])
+		.then(() => {
+			return insertCommentByArticleId(article_id, username, body);
+		})
+		.then((comment) => {
+			res.status(201).send({ comment });
+		})
+		.catch(next);
 };
 
 exports.deleteCommentById = (req, res, next) => {
-    const { comment_id } = req.params;
+	const { comment_id } = req.params;
 
-    removeCommentById(comment_id)
-        .then(() => {
-            res.status(204).send();
-        })
-        .catch(next);
+	removeCommentById(comment_id)
+		.then(() => {
+			res.status(204).send();
+		})
+		.catch(next);
 };
--- models/users.model.js
+++ models/users.model.js
@@ -1,22 +1,26 @@
 const db = require("../db/connection");
 
-exports.checkUserExists = (username) => {
-    return db
-        .query("SELECT username FROM users WHERE username = $1;", [username])
-        .then(({ rows }) => {
-            if (rows.length === 0) {
-                return Promise.reject({
-                    status: 404,
-                    msg: "404: User Not Found",
-                });
-            }
-        });
+exports.selectUsers = () => {
+	return db
+		.query("SELECT username, name, avatar_url FROM users")
+		.then(({ rows }) => {
+			return rows;
+		});
 };
 
-exports.selectUsers = () => {
-    return db
-        .query("SELECT username, name, avatar_url FROM users")
-        .then(({ rows }) => {
-            return rows;
-        });
+exports.selectUserByUsername = (username) => {
+	return db
+		.query(
+			"SELECT username, name, avatar_url FROM users WHERE username = $1;",
+			[username]
+		)
+		.then(({ rows }) => {
+			if (rows.length === 0) {
+				return Promise.reject({
+					status: 404,
+					msg: "404: User Not Found",
+				});
+			}
+			return rows[0];
+		});
 };
